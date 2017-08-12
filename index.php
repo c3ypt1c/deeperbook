e25dc099f56c08eb564fe9f269c2d2ccf12e359b
C3ypt1c <c3ypt1c@gmail.com>
2017-08-12 20:13:42 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once("config.php");?>
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style/loader.css">
+		<script type="text/javascript" src="main.js"></script>
+		<title>Dollarbook</title>
+	</head>
+
+	<body onload="start()">
+
+		<h1>Test</h1>
+
+	</body>
+</html>
---
+++ ph.py
@@ -0,0 +1,9 @@
+import sys;q=True
+for x in sys.argv[1].lower():
+	e=False
+	for y in sys.argv[2].lower():
+		if x==y:
+			e=True;break
+	q=q and e
+	if not q:break
+print(q)
